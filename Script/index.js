ab337d706bd8c7e5834ce8e557237f7bf1e61918
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-09-22 22:21:09 +0530

    send a get request

--- Script/index.js
+++ Script/index.js
@@ -1,45 +1,59 @@
 import { Student } from "../Model/Student.js";
 
-export class StudentController{
+export class StudentController {
 
-  constructor(){
-    $('#Add-btn').click(function (e) { 
-
+  constructor() {
+    $('#Add-btn').on('click', () => {
+      this.saveStudent();
     });
   }
 
   dataValidation() {
-      var name = new RegExp('^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$');
-      var address = new RegExp('^([a-zA-Z\u0080-\u024F]+(?:. |-| |\'))*[a-zA-Z\u0080-\u024F]*$');
-      var email = new RegExp('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$');
-      return !(name.test($('#name').val())) ? false : !address.test($('#city').val()) ? false : !email.test($('#email').val()) ? false : true;
+    var name = new RegExp("^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$");
+    var address = new RegExp(
+      "^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$"
+    );
+    var email = new RegExp(
+      "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$"
+    );
+    return !name.test($("#name").val())
+      ? false
+      : !address.test($("#city").val())
+      ? false
+      : !email.test($("#email").val())
+      ? false
+      : true;
   }
 
-  saveStudent(){
+  saveStudent() {
 
-     const studentObj=JSON.stringify(new Student(
-        $('#name').val(),
-        $('#city').val(),
-        $('#email').val(),
-        $('#level').val()
-      ));
+    const studentObj = JSON.stringify(
+      new Student(
+        $("#name").val(),
+        $("#city").val(),
+        $("#email").val(),
+        $("#level").val()
+      )
+    );
 
-      $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        url: "url",
-        data:studentObj,
-        success: function (response) {
+    console.log(studentObj);
+
+    $.ajax({
+      url: "http://localhost:8080/student/api/hi",
+      type: "GET",
+      data: "",
+      dataType: "",
+      headers: {
+          // "Content-Type": "application/json"
+      },
+      success: (response) => {
           console.log(response);
-        },
-        error:(message)=>{
+      },
+      error: (message) => {
           console.log(message);
-        }
-      });
-
+      }
+  });
   }
-
 }
 
 new StudentController();

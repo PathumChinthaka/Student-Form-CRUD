0b9524093318194fa948b81a631257a2fe608a0b
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-09-24 19:05:44 +0530

    update converter class

--- src/main/java/lk/demo/studentform/studentform/service/impl/StudentServiceImpl.java
+++ src/main/java/lk/demo/studentform/studentform/service/impl/StudentServiceImpl.java
@@ -3,6 +3,7 @@ package lk.demo.studentform.studentform.service.impl;
 import lk.demo.studentform.studentform.dto.StudentDTO;
 import lk.demo.studentform.studentform.persistence.StudentRepository;
 import lk.demo.studentform.studentform.service.StudentService;
+import lk.demo.studentform.studentform.util.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,9 @@ public class StudentServiceImpl implements StudentService {
 
     @Autowired
     StudentRepository studentRepository;
+
+    @Autowired
+    Converter converter;
 
     @Override
     public StudentDTO saveStudent(StudentDTO studentDTO) {
--- src/main/java/lk/demo/studentform/studentform/util/Converter.java
+++ src/main/java/lk/demo/studentform/studentform/util/Converter.java
@@ -1,10 +1,26 @@
 package lk.demo.studentform.studentform.util;
 
+import lk.demo.studentform.studentform.dto.StudentDTO;
+import lk.demo.studentform.studentform.entity.Student;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Converter {
 
     @Autowired
-    ModelMapper modelMapper;
+   private ModelMapper modelMapper;
+
+    public Converter(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public StudentDTO getStudentDTO(Student student){
+        return modelMapper.map(student,StudentDTO.class);
+    }
+
+    public Student getStudentEntity(StudentDTO studentDTO){
+        return modelMapper.map(studentDTO,Student.class);
+    }
 }

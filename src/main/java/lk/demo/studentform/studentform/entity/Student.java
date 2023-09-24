60e1bec4ca376d4b5198658c936c6f901bdcee9d
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-09-25 02:44:53 +0530

    add spring validation

--- src/main/java/lk/demo/studentform/studentform/entity/Student.java
+++ src/main/java/lk/demo/studentform/studentform/entity/Student.java
@@ -1,10 +1,14 @@
 package lk.demo.studentform.studentform.entity;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.lang.NonNull;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,9 +16,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document("Student")
 public class Student implements SuperEntity {
     @Id
+    @NotNull(message = "Id is important")
     private String sid;
+    @NotEmpty(message = "Name is mandatory")
     private String name;
+    @NotEmpty(message = "City is mandatory")
     private String city;
+    @Email(message = "Email is mandatory")
     private String email;
+    @NotEmpty
     private String level;
 }
